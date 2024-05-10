ec7fdd8728bb082d58bc3855a0a7bd60958cdac2
AlninoDP <alninodp@gmail.com>
2024-05-10 15:10:05 +0700

    change welcome page to landing page

--- lib/views/landing_page/landing_page.dart
+++ lib/views/landing_page/landing_page.dart
@@ -1,8 +1,8 @@
 import 'package:dreampage/components/app_background.dart';
 import 'package:flutter/material.dart';
 
-class WelcomePage extends StatelessWidget {
-  const WelcomePage({super.key});
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
