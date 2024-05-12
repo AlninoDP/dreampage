4076b9622c816810b3036eaf174255e38c2d347f
Laptop Alnino <alnino007@gmail.com>
2024-05-12 13:32:37 +0700

    added method and attribut to navigate in login page

--- lib/views/login/providers/login_page_provider.dart
+++ lib/views/login/providers/login_page_provider.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class LoginPageProvider extends ChangeNotifier {
+  LoginPageProvider({required this.initialPage})
+      : pageController = PageController(initialPage: initialPage);
+
+  final PageController pageController;
   bool obscureText = true;
+  int initialPage;
 
   void changePasswordVisibility() {
     obscureText = !obscureText;
+    notifyListeners();
+  }
+
+  void navigateToPage(int index) {
+    pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.ease,
+    );
     notifyListeners();
   }
 }
