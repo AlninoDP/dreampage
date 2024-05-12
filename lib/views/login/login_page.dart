c690fd5031d9a24c320b30a4439aeceae8ca2ba9
Laptop Alnino <alnino007@gmail.com>
2024-05-12 13:32:08 +0700

    added login page to wrap sign in and sign up page

---
+++ lib/views/login/login_page.dart
@@ -0,0 +1,45 @@
+import 'package:dreampage/views/login/providers/login_page_provider.dart';
+import 'package:dreampage/views/login/sign_in/sign_in_page.dart';
+import 'package:dreampage/views/login/sign_up/sign_up_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LoginPageWrapper extends StatelessWidget {
+  const LoginPageWrapper({
+    super.key,
+    required this.initialPage,
+  });
+  final int initialPage;
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => LoginPageProvider(initialPage: initialPage),
+      child: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: true,
+      child: Scaffold(
+        body: Consumer<LoginPageProvider>(
+          builder: (context, provider, child) {
+            return PageView(
+              controller: provider.pageController,
+              onPageChanged: (index) => provider.navigateToPage(index),
+              children: const [
+                SignInPage(),
+                SignUpPage(),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
