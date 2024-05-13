686776922b886caaa38195650aef543b59b36815
AlninoDP <alninodp@gmail.com>
2024-05-13 15:10:28 +0700

    added books models

--- lib/models/books.dart
+++ lib/models/books.dart
@@ -1,1 +1,18 @@
-class Books {}
+class Books {
+  final String title;
+  final String author;
+  final String coverImgSrc;
+  final ReadingStatus status;
+
+  Books({
+    required this.title,
+    required this.author,
+    required this.coverImgSrc,
+    this.status = ReadingStatus.unread,
+  });
+}
+
+enum ReadingStatus {
+  read,
+  unread,
+}
