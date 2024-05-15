b33ecfdfaba825613c26ca0cc6061ceaad19e285
Laptop Alnino <alnino007@gmail.com>
2024-05-15 11:37:41 +0700

    change books model

--- lib/models/books.dart
+++ lib/models/books.dart
@@ -3,6 +3,8 @@ class Books {
   final String title;
   final String author;
   final String coverImgSrc;
+  final String bookImgSrc;
+  final String synopsis;
   final bool isFavorite;
   final ReadingStatus status;
 
@@ -11,6 +13,8 @@ class Books {
     required this.title,
     required this.author,
     required this.coverImgSrc,
+    required this.bookImgSrc,
+    required this.synopsis,
     this.isFavorite = false,
     this.status = ReadingStatus.unread,
   });
