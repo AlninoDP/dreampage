f6bfea3091ccfc85e156567eb501a919642baf54
Laptop Alnino <alnino007@gmail.com>
2024-05-15 11:12:47 +0700

    added book detail page

--- lib/views/book_detail/book_detail_page.dart
+++ lib/views/book_detail/book_detail_page.dart
@@ -1,3 +1,5 @@
+import 'package:dreampage/models/books.dart';
+import 'package:dreampage/views/book_detail/widgets/read_button.dart';
 import 'package:flutter/material.dart';
 
 const testTxt = '''
@@ -8,102 +10,100 @@ Jika demikian, Anda mungkin memerlukan detoksifikasi dopamin.
 ''';
 
 class BookDetailPage extends StatelessWidget {
-  const BookDetailPage({super.key});
+  const BookDetailPage({
+    super.key,
+    required this.books,
+  });
+  final Books books;
 
   @override
   Widget build(BuildContext context) {
     final coverImgHeight = MediaQuery.of(context).size.height * 0.6;
     return Scaffold(
       body: Stack(
-        clipBehavior: Clip.none,
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.6,
-            width: MediaQuery.of(context).size.width,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image:
-                    AssetImage('assets/images/cover_books/dopamine_cover.png'),
-                fit: BoxFit.cover,
+          // Cover Image
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.6,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(books.coverImgSrc),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
+
+          // Synopsis Content
           Positioned(
-            top: coverImgHeight - 100,
+            top: coverImgHeight - 110,
+            left: 0,
+            right: 0,
+            bottom: 0,
             child: Container(
-              padding: const EdgeInsets.all(20),
-              height: MediaQuery.of(context).size.height * 0.6,
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+              height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
               decoration: const BoxDecoration(
-                color: Colors.red,
+                color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(40),
                   topRight: Radius.circular(40),
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 30),
-                  const Text(
-                    'Dopamine Detox',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-                  ),
-                  const Text(
-                    'Thibaut Meurisse',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Color(0xff7D848D),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 30),
+                    Text(
+                      books.title,
+                      style: const TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.w600),
                     ),
-                  ),
-                  const SizedBox(height: 25),
-                  const Text(
-                    'Synopsis',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-
-                  // Texy Synopsis
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 200,
-                    padding: const EdgeInsets.all(10),
-                    color: Colors.white,
-                    child: const SingleChildScrollView(
-                      child: Text(
-                        testTxt,
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xff7D848D),
-                        ),
+                    Text(
+                      books.author,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Color(0xff7D848D),
                       ),
                     ),
-                  ),
-
-                  // Read Button
-
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffA28D4F),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
+                    const SizedBox(height: 25),
+                    const Text(
+                      'Synopsis',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
                       ),
-                      child: Center(
+                    ),
+
+                    // Texy Synopsis
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 200,
+                      padding: const EdgeInsets.all(10),
+                      color: Colors.white,
+                      child: const SingleChildScrollView(
                         child: Text(
-                          'READ',
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 16),
+                          testTxt,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xff7D848D),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+
+                    const SizedBox(height: 30),
+                    // Read Button
+                    const ReadButton()
+                  ],
+                ),
               ),
             ),
           ),
